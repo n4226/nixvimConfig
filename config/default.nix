f851b78bf3ce47308d9bb478c51f9cec0647dcec
Michael Baron <mike.init246@gmail.com>
2024-11-02 22:30:25 -0400

    basic working configuration

--- config/default.nix
+++ config/default.nix
@@ -2,10 +2,59 @@
   # Import all your configuration modules here
   imports = [ ./bufferline.nix ];
 
-  colorschemes.gruvbox.enable = true;
+  config = {
+  #colorschemes.gruvbox.enable = true;
 
   plugins = {
     lualine.enable = true;
   };
+
+  globals.mapleader = " ";
+
+ opts = {
+       	number = true;
+       	relativenumber = true;
+       	shiftwidth = 2;
+ };
+
+
+plugins.telescope = {
+    enable = true;
+    keymaps = {	
+    	"<leader>ff" = "find_files";
+      "<leader>fg" = "live_grep";
+      "<C-p>" = {
+        action = "git_files";
+        options = {
+          desc = "Telescope Git Files";
+        };
+      };
+    };
+    extensions.fzf-native = { enable = true; };
+  };
+
+
+plugins.lsp = {
+  enable = true;
+
+  servers = {
+    lua_ls.enable = true;
+    clangd.enable = true;
+  };
+
+};
+
+plugins.cmp = {
+  enable = true;
+  autoEnableSources = true;
+  settings.sources = [
+    {name = "nvim_lsp";}
+  ];
+
+};
+    # {name = "path";}
+    # {name = "buffer";}
+
+};
 
 }
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+int add(int a, int b) {
+  return a + b;
+}
+
+int main() {
+  printf("Hello\n");
+  int c = add(2,4);
+  return 0;
+}
