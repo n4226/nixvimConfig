6f3cc0596c8c9fffdd31ebe57672205355a0e612
Michael Baron <developer.michael.baron@gmail.com>
2025-04-25 18:29:37 -0400

    add tab control

--- config/default.nix
+++ config/default.nix
@@ -247,6 +247,51 @@
 	action = "<C-w>v";
 	options.desc = "Create new vertical split";
       }
+
+      {
+	mode = "n";
+	key = "<A-L>";
+	action = "<C-w>l";
+	options.desc = "Go to right split";
+	options = {
+	  noremap = true;
+	  silent = true;
+	};
+      }
+      {
+	mode = "n";
+	key = "<A-H>";
+	action = "<C-w>h";
+	options.desc = "Go to left split";
+	options = {
+	  noremap = true;
+	  silent = true;
+	};
+      }
+      {
+	mode = "n";
+	key = "<A-J>";
+	action = "<C-w>j";
+	options.desc = "Go to down split";
+	options = {
+	  noremap = true;
+	  silent = true;
+	};
+      }
+      {
+	mode = "n";
+	key = "<A-K>";
+	action = "<C-w>k";
+	options.desc = "Go to up split";
+	options = {
+	  noremap = true;
+	  silent = true;
+	};
+      }
+      
+
+
+
       {
 	mode = "n";
 	key = "<leader>td";
