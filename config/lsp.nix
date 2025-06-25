cae949e8a51abc4725ab65b8373650a2b4bc41c4
Michael Baron <mbaron2@apple.com>
2025-06-24 17:02:43 -0700

    disable autoformat on file safe

--- config/lsp.nix
+++ config/lsp.nix
@@ -8,26 +8,26 @@
         nix = [ "nixfmt" ];
         cpp = [ "clang-format" ];
       };
-      format_on_save = # Lua
-        ''
-          function(bufnr)
-            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-              return
-            end
-
-            -- if slow_format_filetypes[vim.bo[bufnr].filetype] then
-            --   return
-            -- end
-
-            local function on_format(err)
-              if err and err:match("timeout$") then
-                slow_format_filetypes[vim.bo[bufnr].filetype] = true
-              end
-            end
-
-            return { timeout_ms = 200, lsp_fallback = true }, on_format
-          end
-        '';
+      # format_on_save = # Lua
+      #   ''
+      #     function(bufnr)
+      #       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      #         return
+      #       end
+      #
+      #       -- if slow_format_filetypes[vim.bo[bufnr].filetype] then
+      #       --   return
+      #       -- end
+      #
+      #       local function on_format(err)
+      #         if err and err:match("timeout$") then
+      #           slow_format_filetypes[vim.bo[bufnr].filetype] = true
+      #         end
+      #       end
+      #
+      #       return { timeout_ms = 200, lsp_fallback = true }, on_format
+      #     end
+      #   '';
     };
   };
   keymaps = [
